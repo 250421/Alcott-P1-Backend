c68ca637cc0fa3499b4bfa139edfe43df695308d
Vincent <vealcott1@gmail.com>
2025-04-24 07:36:13 -0500

    Fixed a naming conflict

--- src/main/java/org/revature/Alcott_P1_Backend/service/SessionService.java
+++ src/main/java/org/revature/Alcott_P1_Backend/service/SessionService.java
@@ -1,17 +1,17 @@
 package org.revature.Alcott_P1_Backend.service;
 
-import org.revature.Alcott_P1_Backend.repository.SessionRepository;
+import org.revature.Alcott_P1_Backend.repository.CustomSessionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SessionService {
 
-    private SessionRepository sessionRepository;
+    private CustomSessionRepository customSessionRepository;
 
     @Autowired
-    public SessionService(SessionRepository sessionRepository){
-        this.sessionRepository = sessionRepository;
+    public SessionService(CustomSessionRepository customSessionRepository){
+        this.customSessionRepository = customSessionRepository;
     }
 
     public boolean authenticate(){
